51b9434ed1f0e6b0783235deb962e7fd10bf6c77
Sujan Maharjan <sujan.mhrzn2@gmail.com>
2021-04-29 20:05:50 +0545

    Custom exception message introduced and implemented

--- src/main/java/fonepay/task/ODSBE/exception/ApiExceptionModel.java
+++ src/main/java/fonepay/task/ODSBE/exception/ApiExceptionModel.java
@@ -1,2 +1,16 @@
-package fonepay.task.ODSBE.exception;public class ApiExceptionModel {
+package fonepay.task.ODSBE.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ApiExceptionModel {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime timestamp;
 }
