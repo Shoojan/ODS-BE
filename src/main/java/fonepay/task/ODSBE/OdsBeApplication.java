3283c83700f6acd20c2a3ac8dec7463e48c9262c
Sujan Maharjan <sujan.mhrzn2@gmail.com>
2021-04-29 16:23:26 +0545

    BE service project created for Online Delivery System

---
+++ src/main/java/fonepay/task/ODSBE/OdsBeApplication.java
@@ -0,0 +1,13 @@
+package fonepay.task.ODSBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fonepay/task/ODSBE/OdsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package fonepay.task.ODSBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
